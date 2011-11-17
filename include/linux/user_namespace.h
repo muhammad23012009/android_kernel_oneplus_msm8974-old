5fa51565bb56ef828108fc5ab12a054d748fec41
Eric W. Biederman <ebiederm@xmission.com>
2011-11-17 01:32:59 -0800

    userns: Simplify the user_namespace by making userns->creator a kuid.
    - Transform userns->creator from a user_struct reference to a simple
      kuid_t, kgid_t pair.
    
      In cap_capable this allows the check to see if we are the creator of
      a namespace to become the classic suser style euid permission check.
    
      This allows us to remove the need for a struct cred in the mapping
      functions and still be able to dispaly the user namespace creators
      uid and gid as 0.
    
    - Remove the now unnecessary delayed_work in free_user_ns.
    
      All that is left for free_user_ns to do is to call kmem_cache_free
      and put_user_ns.  Those functions can be called in any context
      so call them directly from free_user_ns removing the need for delayed work.
    
    Acked-by: Serge Hallyn <serge.hallyn@canonical.com>
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>

--- include/linux/user_namespace.h
+++ include/linux/user_namespace.h
@@ -13,8 +13,8 @@ struct user_namespace {
 	struct kref		kref;
 	struct hlist_head	uidhash_table[UIDHASH_SZ];
 	struct user_namespace	*parent;
-	struct user_struct      *creator;
-	struct work_struct      destroyer;
+	kuid_t			owner;
+	kgid_t			group;
 	unsigned int		proc_inum;
 };
 
--- kernel/user_namespace.c
+++ kernel/user_namespace.c
@@ -51,6 +51,16 @@ int create_user_ns(struct cred *new)
 	struct user_struct *root_user;
 	int n;
 	int ret;
+	kuid_t owner = make_kuid(new->user_ns, new->euid);
+	kgid_t group = make_kgid(new->user_ns, new->egid);
+
+	/* The creator needs a mapping in the parent user namespace
+	 * or else we won't be able to reasonably tell userspace who
+	 * created a user_namespace.
+	 */
+	if (!kuid_has_mapping(parent_ns, owner) ||
+	    !kgid_has_mapping(parent_ns, group))
+		return -EPERM;
 
 	ns = kmem_cache_alloc(user_ns_cachep, GFP_KERNEL);
 	if (!ns)
@@ -73,29 +83,14 @@ int create_user_ns(struct cred *new)
 	return 0;
 }
 
-/*
- * Deferred destructor for a user namespace.  This is required because
- * free_user_ns() may be called with uidhash_lock held, but we need to call
- * back to free_uid() which will want to take the lock again.
- */
-static void free_user_ns_work(struct work_struct *work)
-{
-	struct user_namespace *parent, *ns =
-		container_of(work, struct user_namespace, destroyer);
-	parent = ns->parent;
-	free_uid(ns->creator);
-	proc_free_inum(ns->proc_inum);
-	kmem_cache_free(user_ns_cachep, ns);
-	put_user_ns(parent);
-}
-
 void free_user_ns(struct kref *kref)
 {
-	struct user_namespace *ns =
+	struct user_namespace *parent, *ns =
 		container_of(kref, struct user_namespace, kref);
 
-	INIT_WORK(&ns->destroyer, free_user_ns_work);
-	schedule_work(&ns->destroyer);
+	parent = ns->parent;
+	proc_free_inum(ns->proc_inum);
+	put_user_ns(parent);
 }
 EXPORT_SYMBOL(free_user_ns);
 
@@ -106,12 +101,11 @@ uid_t user_ns_map_uid(struct user_namespace *to, const struct cred *cred, uid_t 
 	if (likely(to == cred->user_ns))
 		return uid;
 
-
 	/* Is cred->user the creator of the target user_ns
 	 * or the creator of one of it's parents?
 	 */
 	for ( tmp = to; tmp != &init_user_ns; tmp = tmp->parent ) {
-		if (cred->user == tmp->creator) {
+		if (uid_eq(cred->user->uid, tmp->owner)) {
 			return (uid_t)0;
 		}
 	}
@@ -131,7 +125,7 @@ gid_t user_ns_map_gid(struct user_namespace *to, const struct cred *cred, gid_t 
 	 * or the creator of one of it's parents?
 	 */
 	for ( tmp = to; tmp != &init_user_ns; tmp = tmp->parent ) {
-		if (cred->user == tmp->creator) {
+		if (uid_eq(cred->user->uid, tmp->owner)) {
 			return (gid_t)0;
 		}
 	}
